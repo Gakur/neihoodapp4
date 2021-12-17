e08f9322c290fead002db6a73180a5c4f9b9061c
Peter Gakure <petergakure97@gmail.com>
2021-12-17 15:00:11 +0300

    Added a new database

--- hoods/settings.py
+++ hoods/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'hoods.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neigh4',
+        'USER': 'pjnr',
+        'PASSWORD': 'peter1010',
     }
 }
 
