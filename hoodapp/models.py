f9f17a4e879f2351302d883bbc42c0a5a5acb181
Peter Gakure <petergakure97@gmail.com>
2021-12-17 14:12:59 +0300

    Added post model

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -27,3 +27,22 @@ class Profile(models.Model):
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
+
+class Post(models.Model):
+    title = models.CharField(max_length=120, null=True)
+    post = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='post_owner')
+    hood = models.ForeignKey('Neighbourhood', on_delete=models.CASCADE, related_name='hood_post')
+
+
+    def __str__(self):
+        return f'{self.title} Post'
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+    
+
