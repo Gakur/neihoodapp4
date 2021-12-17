529b99cab671c263827bed24019952b8c6372dfe
Peter Gakure <petergakure97@gmail.com>
2021-12-17 14:19:14 +0300

    Added the index view class

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from .forms import BusinessForm, ProfileForm, HoodForm, PostForm
+from django.contrib.auth.decorators import login_required
+from .models import Neighbourhood, Business, Post 
+
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    return render(request, 'index.html')
